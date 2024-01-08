f48904e27419f922cc8e6be5d605235ea2a6d739
nvoers <nick-vanoers@hotmail.com>
2024-01-08 16:53:29 +0100

    pictures

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
         <main className="bg-neutral-50 text-black selection:bg-pink-600 dark:bg-neutral-900 dark:text-white dark:selection:text-white h-screen">
             <Header />
             <div className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-3 md:grid-rows-1">
-                <div className="md:col-span-1 md:row-span-1 overflow-hidden rounded-lg relative">
+                <div className="md:col-span-1 md:row-span-1 overflow-hidden rounded-lg relative border-2 border-black">
                     <Link href={"/order"}>
-                        <Image src="/mockups/shirt.png" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out" width={1000} height={1000} />
+                        <Image src="/mockups/shirtsquare.png" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out" width={1000} height={1000} />
                         <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-cyan-200/80 to-pink-600/80 opacity-0 transition duration-300 ease-in-out hover:opacity-100 text-4xl text-white font-semibold content-center grid">
                             <p className="text-center">T-SHIRT</p>
                         </div>
@@ -20,9 +20,9 @@ export default function Home() {
                     </Link>
                 </div>
                 
-                <div className="md:col-span-1 md:row-span-1 overflow-hidden rounded-lg relative">
+                <div className="md:col-span-1 md:row-span-1 overflow-hidden rounded-lg relative border-2 border-black">
                     <Link href={"/order"}>
-                        <Image src="/mockups/sweater.png" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out" width={1000} height={1000} />
+                        <Image src="/mockups/sweatersquare.png" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out" width={1000} height={1000} />
                         <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-cyan-200/80 to-pink-600/80  opacity-0 transition duration-300 ease-in-out hover:opacity-100 text-4xl text-white font-semibold content-center grid">
                             <p className="text-center">SWEATER</p>
                         </div>
@@ -31,9 +31,9 @@ export default function Home() {
                         </div>
                     </Link>
                 </div>
-                <div className="md:col-span-1 md:row-span-1 overflow-hidden rounded-lg relative">
+                <div className="md:col-span-1 md:row-span-1 overflow-hidden rounded-lg relative border-2 border-black">
                     <Link href={"/order"}>
-                        <Image src="/mockups/shirt.png" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out" width={1000} height={1000} />
+                        <Image src="/mockups/shirtsquare.png" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out" width={1000} height={1000} />
                         <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-cyan-200/80 to-pink-600/80 opacity-0 transition duration-300 ease-in-out hover:opacity-100 text-4xl text-white font-semibold content-center grid">
                             <p className="text-center">SPORT SHIRT</p>
                         </div>
