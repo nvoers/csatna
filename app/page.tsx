7815824194a6dc705d9c94f3b5ef5fbf2d45e188
nvoers <nick-vanoers@hotmail.com>
2024-01-08 11:59:52 +0100

    base site

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-      
+    <main className="bg-neutral-50 text-black selection:bg-pink-600 dark:bg-neutral-900 dark:text-white dark:selection:text-white">
+      <div className="navbar">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl">CSATNA</Link>
+          <Link href="/" className="text-neutral-500 px-4 hover:text-black hover:underline dark:hover:text-black">Order</Link>
+          <Link href="/" className="text-neutral-500 px-4 hover:text-black hover:underline">CSATNA x BATA</Link>
+        </div>
+      </div>
+
+      <div className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-2">
+        <div className="md:col-span-4 md:row-span-2 overflow-hidden rounded-lg hover:border-2 hover:border-black">
+          <Image src="/posters/IMG_0808.jpg" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out" width={1000} height={1000} />
+        </div>
+        <div className="md:col-span-2 md:row-span-1 overflow-hidden rounded-lg hover:border-2 hover:border-black">
+          <Image src="/posters/IMG_0803.jpg" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out w-full" width={500} height={500} />
+        </div>
+        <div className="md:col-span-2 md:row-span-1 overflow-hidden rounded-lg hover:border-2 hover:border-black">
+          <Image src="/posters/IMG_0817.jpg" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out w-full" width={500} height={500} />
+        </div>
+      </div>
+      <div className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-1">
+        <div className="md:col-span-3 md:row-span-1 overflow-hidden rounded-lg hover:border-2 hover:border-black">
+          <Image src="/fotos/0062.jpg" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out" width={1000} height={500} />
+        </div>
+        <div className="md:col-span-3 md:row-span-1 overflow-hidden rounded-lg hover:border-2 hover:border-black">
+          <Image src="/fotos/confessions.jpeg" alt="CSATNA" className="rounded-lg hover:scale-105 transition duration-300 ease-in-out h-full" width={1000} height={500} />
+        </div>
+      </div>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,20 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#ffffff",
+          "base-100": "#ffffff",
+          "link": "#737373",
+        },
+      },
+    ],
+  },
 }
 export default config
