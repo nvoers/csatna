bd31d5152f4d97729985dd9b9138949dea952e1b
Nick van Oers <39125833+nvoers@users.noreply.github.com>
2024-04-11 14:43:28 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,6 @@ export default function Header() {
         <div className="navbar">
             <div className="flex-1">
                 <Link href={"/"} className="btn btn-ghost text-xl">CSATNA</Link>
-                <Link href={"/products"} className="text-neutral-500 px-4 hover:text-black hover:underline dark:hover:text-black">Products</Link>
             </div>
         </div>
     );
